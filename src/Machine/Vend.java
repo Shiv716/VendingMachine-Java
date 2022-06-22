9a2d90af07db130c4aa5038d3ff98c6843c59290
S-Prince7 <101105459+S-Prince7@users.noreply.github.com>
2022-06-22 22:01:17 +0530

    Vending GUI

---
+++ src/Machine/Vend.java
@@ -0,0 +1,15 @@
+package Machine;
+
+public class Vend {
+    
+    // Enter a number
+    // Check if the product exists with that code
+    // If no , then ask the customer to re-verify it . Yes: Enter the correct code , No : Exit option
+    // After selecting the product , ask for card details
+    // Verify the details , if the format is correct , move on to verify with the bank .
+    // If the details are correct : Make the payment and drop the product . IF NOT , ask to re-enter the details -
+    // - at max two times and then exit the process (also give the option to exit manually) .
+    //  Let's makeup a GUI for this for better usage.
+
+
+}
